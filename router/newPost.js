8c487a5d7b5f0f9f24a287d9de0e7f51743c1171
Kaisma Penn-Titley <kptitley@gmail.com>
2017-10-02 11:58:31 -0400

    json file updated with test data | newpost.js: concatenated req.body.firstName & req.body.lastName; commented out newPost.author, newPost.author.firstName & newPost.author.lastName

--- router/newPost.js
+++ router/newPost.js
@@ -13,9 +13,10 @@ router.post("/", function(req, res, next){
     var newPost = {};
     newPost.id = req.body.id;
     newPost.title = req.body.title;
-    newPost.author = {};
-    newPost.author.firstName = req.body.firstName;
-    newPost.author.lastName = req.body.lastName;
+    // newPost.author = {};   
+    newPost.author = req.body.firstName + req.body.lastName;
+    // newPost.author.firstName = req.body.firstName;
+    // newPost.author.lastName = req.body.lastName;
     newPost.author.email = req.body.email;
     newPost.pubDate = req.body.pubDate;
     newPost.content = req.body.content;
